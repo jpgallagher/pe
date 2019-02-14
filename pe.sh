4f15959455313900039dbff12ecdf36af8b2e704
John Gallagher <jpg@ruc.dk>
2019-02-14 17:41:07 +0100

    Added options

--- pe.sh
+++ pe.sh
@@ -2,6 +2,9 @@
 # read -d flag if present for drawing cfp
 # $1 - file
 # $2 - initial goal
+# $3 - property level  
+
+# If $3 not present, then run all property generators
 
 PE="/Users/jpg/Research/LP/clptools/predabs/pe"
 
@@ -25,27 +28,43 @@ if (test ! -d $resultdir) then
         mkdir $resultdir
 fi
 
-#k=4
-#until [[ $k -eq 0 ]];
-#do
-
 k=$3
 if [[ $k -eq 0 ]]; then
-   echo "assuming k = 1"
-   k=1
-fi
+  k=4
+  until [[ $k -eq 0 ]];
+  do
 
-   $PE/props -prg "$1" -l $k -o "$resultdir/$f.props"
+   # Use props property generator k = 1..4
+   $PE/props -prg "$1" -l $k -o "$resultdir/$f$k.props"
+   $PE/peunf_smt_2 -prg "$1" -entry "$2" -props "$resultdir/$f$k.props" -o "$resultdir/$f$k.pe.pl" 
+  
+   if [[ $draw -eq 1 ]]; then
+     $PE/drawcfg -prg "$resultdir/$f$k.pe.pl" -o "$resultdir/cfg.txt"
+     dot -Tjpg -o "$resultdir/cfg$k.jpg" "$resultdir/cfg.txt"
+     rm "$resultdir/cfg.txt"
+   fi
+   k=`expr $k \- 1`
+  done
+   # Finally use props1 property generator
+   $PE/props1 -prg "$1" -entry "$2" -o "$resultdir/$f.props"
    $PE/peunf_smt_2 -prg "$1" -entry "$2" -props "$resultdir/$f.props" -o "$resultdir/$f.pe.pl" 
-   #$PE/peunf_smt -prg "$1" -entry "$2" -props "$resultdir/$f.props" -o "$resultdir/$f.pe.pl" 
-
-   #$PE/peunf -prg "$1" -entry "$2" -props "$resultdir/$f.props" -o "$resultdir/$f.pe.pl" 
-
-
+   k=""
    if [[ $draw -eq 1 ]]; then
      $PE/drawcfg -prg "$resultdir/$f.pe.pl" -o "$resultdir/cfg.txt"
      dot -Tjpg -o "$resultdir/cfg$k.jpg" "$resultdir/cfg.txt"
      rm "$resultdir/cfg.txt"
    fi
-   #k=`expr $k \- 1`
-#done
+else
+   # Use the provided value of k
+   $PE/props -prg "$1" -l $k -o "$resultdir/$f$k.props"
+   $PE/peunf_smt_2 -prg "$1" -entry "$2" -props "$resultdir/$f$k.props" -o "$resultdir/$f$k.pe.pl" 
+  
+   if [[ $draw -eq 1 ]]; then
+     $PE/drawcfg -prg "$resultdir/$f$k.pe.pl" -o "$resultdir/cfg.txt"
+     dot -Tjpg -o "$resultdir/cfg$k.jpg" "$resultdir/cfg.txt"
+     rm "$resultdir/cfg.txt"
+   fi
+fi
+
+
+
